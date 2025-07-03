53bcf9ab3c66a661620cb8ef1401ba3bdf36be0d
Your Name <your-email@example.com>
2025-07-03 22:09:28 +0530

    Initial commit: Java web server with Docker

--- index.java
+++ index.java
@@ -1,6 +1,26 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new java.net.InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started at http://localhost:8080/");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello, World from Java HTTP Server!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
 
